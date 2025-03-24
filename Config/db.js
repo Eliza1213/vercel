93e4f3bb7f04ee3539d42a7a6002712a4bc7501b
Eliza1213 <20230015@uthh.edu.mx>
2025-03-24 02:00:11 -0600

    Descripción del cambio

--- Config/db.js
+++ Config/db.js
@@ -2,14 +2,11 @@ const mongoose = require("mongoose");
 
 const conectarDB = async () => {
   try {
-    await mongoose.connect(process.env.MONGO_URI, {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-    });
+    await mongoose.connect(process.env.MONGO_URI); // Sin opciones adicionales
     console.log("✅ Conectado a MongoDB Atlas");
   } catch (error) {
     console.error("❌ Error al conectar a MongoDB:", error);
-    process.exit(1);
+    process.exit(1); // Finaliza la aplicación si hay un error
   }
 };
 
